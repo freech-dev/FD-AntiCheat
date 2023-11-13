4a71714743d18bdd9a8418273de4c50e7073ca6a
Cian Murray <cianseamusmurray@gmail.com>
2023-11-13 21:52:38 +0000

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -8,7 +8,7 @@ AddEventHandler('explosionEvent', function(sender, ev)
     end
 
     if isExplosionBlacklisted then
-        DropPlayer(sender, "[FD AC] You have triggered a blacklisted explotion.")
+        DropPlayer(sender, "[FD AC] You have triggered a blacklisted explosion.")
         CancelEvent()
     end
 end)
@@ -93,3 +93,36 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+
+async function DoesPlayerHasBlacklistPed(id) {
+    const ped = GetPlayerPed(id)
+    for (const key in pcf) {
+        
+        const plped = GetEntityModel(ped)
+        const hash = GetHashKey(pcf[key])
+
+        if (plped == hash) {
+            emitNet("ChangePed", id)
+        }
+    }
+}
+
+async function DoesPlayerSitInBlacklistVehicle(id) {
+    const ped = GetPlayerPed(id)
+    for (const key in vcf) {
+        const plvehicle = GetVehiclePedIsIn(ped, false)
+        let plmodel = 0
+
+        if (plvehicle != 0) {
+            plmodel = GetEntityModel(plvehicle)
+        }
+
+        const blvehicle = GetHashKey(vcf[key])
+
+        if (plmodel == blvehicle) {
+            const Nid = NetworkGetNetworkIdFromEntity(plvehicle)
+            emitNet("LeaveVehicle", id, Nid)
+        }
+    }
+}
